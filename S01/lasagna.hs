a0a98fbff429495628545e1f299e96a9d6d20c1f
dev-david-alves <davidalves9090@gmail.com>
2022-03-30 19:36:49 -0300

    .gitignore e Primeira semana atualizados.

---
+++ S01/lasagna.hs
@@ -0,0 +1,8 @@
+module LuciansLusciousLasagna (elapsedTimeInMinutes, expectedMinutesInOven, preparationTimeInMinutes) where
+
+-- TODO: define the expectedMinutesInOven constant
+expectedMinutesInOven = 40
+-- TODO: define the preparationTimeInMinutes function
+preparationTimeInMinutes x = x * 2
+-- TODO: define the elapsedTimeInMinutes function
+elapsedTimeInMinutes x y = (preparationTimeInMinutes x) + y
